fc988d48ea92f102ae90bfe3f95665c1f143759c
Gautam Bhuyan <gautam.bhuyan@gmail.com>
2025-08-17 15:29:22 -0400

    Update amazonRAGDataLoad.py

--- amazonRAGDataLoad.py
+++ amazonRAGDataLoad.py
@@ -1,10 +1,13 @@
 import os
 from langchain.documentloaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 
 sourceData = PyPDFLoader("source")
 
-cleanedData = sourceData.load_and_split()
+#cleanedData = sourceData.load_and_split()
 
-print("Length of data = ", len(cleanedData))
+splitData = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", " ", ""], chunk_size=100, chunk_overlap=10, sourceData
+#print("Length of data = ", len(cleanedData))
 
 print("Sample data = ", cleanedData[0])
